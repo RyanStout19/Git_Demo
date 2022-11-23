2da831ec8b687f272d3c50948ca3b28df2aaa6d8
Ryan Stout <ryan.stout@bedegaming.com>
2022-11-23 13:53:04 +0000

    test_script and workflow

---
+++ test_script.sh
@@ -0,0 +1,13 @@
+# shell script that operates the same way as writing in terminal
+
+#shebang which tells this script how we want it to run
+#! /bin/bash 
+
+echo "Hello World!"
+pwd
+whoami
+ls -a
+cat README.md
+touch "wag1" >> scriptfile.txt
+echo scriptfile.txt
+
